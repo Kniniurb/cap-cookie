e1d69b145f7b1393df361cfe737f13e9122ebd4b
Peter Bruinink <peter@plusot.com>
2023-08-23 19:38:12 +0200

    downgrade

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,15 +10,23 @@ export class HomePage {
   key = '';
   value = '';
 
+  constructor() {
+    document.cookie = `test=${new Date().toISOString()}`;
+  }
+
   async addCookie(): Promise<void> {
     const key = this.key.trim();
     const value = this.value.trim();
     if (key && value) {
       await CapacitorCookies.setCookie({
         url: 'http://localhost',
-        key,
-        value,
+        key: key,
+        value: `${value} ${new Date().toISOString()}`,
       });
+      CapacitorCookies.getCookies().then((cookies) => {
+        console.log('Cookies', cookies);
+        alert('Cookies: ' + JSON.stringify(cookies));
+      });  
     }
   }
 
